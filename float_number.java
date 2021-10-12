b267a5dcac727b93dd2e5fa423a6d7cee29b4271
joshiabhishek2030 <92031328+joshiabhishek2030@users.noreply.github.com>
2021-10-12 13:15:43 +0530

    Create float_number.java

---
+++ float_number.java
@@ -0,0 +1,23 @@
+
+public class float_number {
+	public static void main(String args[] {
+		byte my_age=29;
+		short my_salary=10000;
+		int pincode=834001;
+		long my_phone=9394849929283L;
+		float percentage=91.4f;
+		double annualsalary=273727273.3848482929d;
+		boolean isIndian=true;
+		char section='B';{
+		
+		System.out.println(my_age);
+		System.out.println(my_salary);
+		System.out.println(pincode);
+		System.out.println(my_phone);
+		System.out.println(percentage);
+		System.out.println(annualsalary);
+		System.out.println(isIndian);
+		System.out.println(section);
+	}
+
+}
