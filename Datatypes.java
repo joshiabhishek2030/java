19555486bbbf6aee70f49b163f757ad9bba51af0
joshiabhishek2030 <92031328+joshiabhishek2030@users.noreply.github.com>
2021-10-12 13:06:22 +0530

    Create Datatypes.java

---
+++ Datatypes.java
@@ -0,0 +1,50 @@
+package precourse_java;
+
+public class Datatypes {
+
+	public static void main(String args[]) {
+		byte my_age = 21;
+		short my_salary=10000;
+		int pincode = 497229;
+		long my_phone = 9981894800L;
+				
+		float percentage=94.5f;
+		double anuual_salary=2847284.2412752969d;
+		
+		boolean isIndian = true;
+		char Section='B';
+		
+		String my_name="Ujjwal";
+		
+		System.out.println(my_age);
+		System.out.println(my_salary);
+		System.out.println(pincode);
+		System.out.println(my_phone);
+		System.out.println(percentage);
+		System.out.println(anuual_salary);
+		System.out.println(isIndian);
+		System.out.println(Section);
+		System.out.println(my_name);
+		
+	}
+}
+/*
+ * data_type name_of_variable=Data/value
+ * 
+ * if variable is the container ,data_type is a type of the container
+ * 
+ */
+// a variable can have 
+// A-Z,a-z,0-9,_,$
+// not valid ! @ # % & --
+// There is no space between the variable name.
+// The name of the variable cannot start with number.
+
+//Acceptable
+// _abdhe48
+// $chdb4
+
+// Not Acceptable
+// 9cjdbch
+// #cjdn
+// My space
