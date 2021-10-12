c4fae6bd50560cd77ef98ff181c198cfb3d77331
joshiabhishek2030 <92031328+joshiabhishek2030@users.noreply.github.com>
2021-10-12 13:09:09 +0530

    Create SumofEvenNumber.java

---
+++ SumofEvenNumber.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+
+ public class SumofEvenNumber {
+     public static void main(String args[]){
+         
+         int sum=0;
+         int n;
+         Scanner sc =new Scanner(System.in);
+         System.out.println("Enter the number:"); 
+            n = sc.nextInt();
+         for(int i=1;i<=n;i++)
+         {
+           if(i%2==0)
+           {
+               sum= sum+i;              
+           }
+         }
+         System.out.print(sum+ " is the sum of even numbers" );
+     }
+ }
