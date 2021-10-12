51ac0a65a08555e2c608e8ef1a893c3b53863abc
joshiabhishek2030 <92031328+joshiabhishek2030@users.noreply.github.com>
2021-10-12 13:05:18 +0530

    Create Hello_World.java

---
+++ Hello_World.java
@@ -0,0 +1,11 @@
+package precourse_java;
+
+public class Hello_World {
+	//function/method
+	public static void main(String args[]) {
+		System.out.println("hello world");
+		System.out.println("welcome");
+		System.out.print("Java");
+	}
+	
+}
